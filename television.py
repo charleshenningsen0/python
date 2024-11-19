7b75df43d6fd5a2bb64c6f634d82b4aa62a26f02
charleshenningsen0 <henningsencj124@gmail.com>
2024-11-19 13:09:21 -0600

    added a bunch of stuff

--- television.py
+++ television.py
@@ -1,3 +1,51 @@
 
-class Television():
-    pass
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
+
+    def power(self):
+        self.__status = not self.__status
+
+    def mute(self):
+        self.__muted = not self.__muted
+
+    def channel_up(self):
+        if self.__channel == self.MAX_CHANNEL:
+            self.__channel = self.MIN_CHANNEL
+        else:
+            self.__channel += 1
+
+    def channel_down(self):
+        if self.__channel == self.MIN_CHANNEL:
+            self.__channel = self.MAX_CHANNEL
+        else:
+            self.__channel -= 1
+
+    def volume_up(self):
+        if self.__muted == True:
+            self.mute()
+
+        if self.__volume == self.MAX_VOLUME:
+            self.__volume = self.MAX_VOLUME
+        else:
+            self.__volume += 1
+
+    def volume_down(self):
+        if self.__muted == True:
+            self.mute()
+
+        if self.__volume == self.MIN_VOLUME:
+            self.__volume = self.MAX_VOLUME
+        else:
+            self.__volume -= 1
+
+    def __str__(self):
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
