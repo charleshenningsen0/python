66620fe0f39b9e572e420db3ff497613293dcae0
charleshenningsen0 <henningsencj124@gmail.com>
2024-11-20 11:10:36 -0600

    Added pytest test and some docstrings and one type hinting

--- television.py
+++ television.py
@@ -13,14 +13,29 @@ class Television:
         self.__previousVolume = self.__volume
 
     def power(self):
+        '''
+        Changes truth value of __status
+        :param self: television object
+        :return: void
+        '''
         self.__status = not self.__status
 
     def mute(self):
+        '''
+        Changes truth value of __muted and swaps __previousVolume with __volume
+        :param self: television object
+        :return: void
+        '''
         if self.__status == True:
             self.__muted = not self.__muted#switch
             self.__previousVolume, self.__volume = self.__volume, self.__previousVolume
 
     def channel_up(self):
+        '''
+        When on, channel number if max, then changes to zero, otherwise up by one
+        :param self: television object
+        :return: void
+        '''
         if self.__status == True:
             if self.__channel == self.MAX_CHANNEL:
                 self.__channel = self.MIN_CHANNEL
@@ -28,6 +43,11 @@ class Television:
                 self.__channel += 1
 
     def channel_down(self):
+        '''
+        When on, changes channel number if min, then changes to max, otherwise down by one
+        :param self: television object
+        :return: void
+        '''
         if self.__status == True:
             if self.__channel == self.MIN_CHANNEL:
                 self.__channel = self.MAX_CHANNEL
@@ -35,18 +55,26 @@ class Television:
                 self.__channel -= 1
 
     def volume_up(self):
+        '''
+        When on, if muted call mute(), then if max, no change, otherwise add volume by one
+        :param self: television object
+        :return: void
+        '''
         if self.__status == True:#if on
             if self.__muted == True:
                 self.mute()
 
             if self.__volume == self.MAX_VOLUME:
-                self.__volume = self.MAX_VOLUME
+                pass
             else:
                 self.__volume += 1
 
     def volume_down(self):
-        #maybe always need to -=1
-        #Go line by line, maybe test, see where we are going wrong here
+        '''
+        When on, if muted call mute(), then if max, no change, otherwise add volume by one
+        :param self: television object
+        :return: void
+        '''
         if self.__status == True:#if on
             if self.__muted == False:#if unmuted
                 if self.__volume == self.MIN_VOLUME:
@@ -64,5 +92,10 @@ class Television:
 
 
 
-    def __str__(self):
+    def __str__(self) -> str:
+        '''
+        Returns the state of the television object
+        :param self: television object
+        :return: Television status, channel, and volume
+        '''
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
---
+++ test_television.py
@@ -0,0 +1,102 @@
+import pytest
+from television import *
+
+class Test:
+    def setup_method(self):
+        self.tele1 = Television()
+
+
+    #done after each test
+    def teardown_method(self):
+        del self.tele1
+
+
+    def test_init(self):
+        assert self.tele1.__str__() == "Power = False, Channel = 0, Volume = 0"
+
+    def test_power(self):
+        self.tele1.power()
+        assert self.tele1.__str__() == "Power = True, Channel = 0, Volume = 0"
+
+        self.tele1.power()
+        assert self.tele1.__str__() == "Power = False, Channel = 0, Volume = 0"
+
+    def test_mute(self):
+        #on, up one, and muted
+        self.tele1.power()
+        self.tele1.volume_up()
+        self.tele1.mute()
+        assert self.tele1.__str__() == "Power = True, Channel = 0, Volume = 0"
+
+        # On and unmuted
+        self.tele1.mute()
+        assert self.tele1.__str__() == "Power = True, Channel = 0, Volume = 1"
+
+        # off and muted
+        self.tele1.power()
+        self.tele1.mute()
+        assert self.tele1.__str__() == "Power = False, Channel = 0, Volume = 1"
+
+        # off and unmuted
+        self.tele1.mute()
+        assert self.tele1.__str__() == "Power = False, Channel = 0, Volume = 1"
+
+
+    def test_channel_up(self):
+        self.tele1.channel_up()
+        assert self.tele1.__str__() == "Power = False, Channel = 0, Volume = 0"
+
+        self.tele1.power()
+        self.tele1.channel_up()
+        assert self.tele1.__str__() == "Power = True, Channel = 1, Volume = 0"
+
+        for _ in range(self.tele1.MAX_CHANNEL): # 3
+            self.tele1.channel_up()
+
+        assert self.tele1.__str__() == "Power = True, Channel = 0, Volume = 0"
+
+
+
+    def test_channel_down(self):
+        self.tele1.channel_down()
+        assert self.tele1.__str__() == "Power = False, Channel = 0, Volume = 0"
+
+        self.tele1.power()
+        self.tele1.channel_down()
+        assert self.tele1.__str__() == f'Power = True, Channel = {self.tele1.MAX_CHANNEL}, Volume = 0'
+
+
+
+    def test_volume_up(self):
+        self.tele1.volume_up()
+        assert self.tele1.__str__() == "Power = False, Channel = 0, Volume = 0"
+
+        self.tele1.power()
+        self.tele1.volume_up()
+        assert self.tele1.__str__() == "Power = True, Channel = 0, Volume = 1"
+
+        self.tele1.mute()
+        self.tele1.volume_up()
+        assert self.tele1.__str__() == "Power = True, Channel = 0, Volume = 2"
+
+        for _ in (self.tele1.MAX_VOLUME):
+            self.tele1.volume_up()
+        assert self.tele1.__str__() == f"Power = True, Channel = 0, Volume = {self.tele1.MAX_VOLUME}"
+
+    def test_volume_up(self):
+        self.tele1.volume_down()
+        assert self.tele1.__str__() == "Power = False, Channel = 0, Volume = 0"
+
+        self.tele1.power()
+        for _ in range(self.tele1.MAX_VOLUME):
+            self.tele1.volume_up()
+
+        self.tele1.volume_down()
+        assert self.tele1.__str__() == f"Power = True, Channel = 0, Volume = {self.tele1.MAX_VOLUME - 1}"
+
+        self.tele1.mute()
+        self.tele1.volume_down()
+        assert self.tele1.__str__() == f"Power = True, Channel = 0, Volume = {self.tele1.MAX_VOLUME - 2}"
+
+
+
